9a4043488f1f6ab6dad313e3f5813f8aac16012b
Miroslav Jancarik <miroslav.jancarik@firma.seznam.cz>
2016-09-09 19:02:54 +0200

    #! add workshop1

---
+++ reactjs/startedWorkshops/workshop1/src/__tests__/UIComponentHelperSpec.js
@@ -0,0 +1,17 @@
+describe('UIComponentHelper', () => {
+
+	let windowViewportRect = {
+		top: 0,
+		left: 0,
+		width: 1024,
+		height: 768
+	};
+
+	let elmRect = {
+		top: -70,
+		left: 462,
+		width: 100,
+		height: 100
+	};
+
+});
