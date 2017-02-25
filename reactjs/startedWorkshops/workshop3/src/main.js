9181bb2db39a996550092b187cec09eddc6bc308
Martin Jurča <martin.jurca@firma.seznam.cz>
2017-02-25 13:54:34 +0100

    added workshop 3

---
+++ reactjs/startedWorkshops/workshop3/src/main.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import MovieListing from './page/MovieListing.jsx';
+
+ReactDOM.render(
+	<MovieListing/>,
+	document.getElementById('root')
+);
