2acf722ecafa1a6cabaac138189e8eb777bbfa14
Martin Jurča <martin.jurca@firma.seznam.cz>
2018-04-07 15:42:55 +0200

    added workshop 2 (completed tests)

---
+++ react-ecosystem/workshop2/ui/atom/Sizer.js
@@ -0,0 +1,25 @@
+import classnames from 'classnames';
+import PropTypes from 'prop-types';
+import React from 'react';
+
+export default function Sizer({width, height, placeholder}) {
+  return (
+    <div
+      className={classnames(
+        'atm-sizer',
+        {
+          'atm-placeholder': placeholder,
+        },
+      )}
+      style={{
+        paddingTop: `${height / width * 100}%`,
+      }}
+    />
+  );
+}
+
+Sizer.propTypes = {
+  width: PropTypes.number.isRequired,
+  height: PropTypes.number.isRequired,
+  placeholder: PropTypes.bool,
+};
